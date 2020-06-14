996fc6f4b78e1997d0bf5a0ce3e7fc4b85c7c7ce
Gregory Singleton <yadig69design@gmail.com>
2020-06-14 21:13:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = "I can't hear you"
+  var uppercase = "YES INDEED!"
+  var lovegrandma = "I love you, to"
+  if (lowercase.toLowerCase(string) === string) {
+    return lowercase;
+  }
+  else if (uppercase.toUpperCase(string) === string) {
+    return uppercase;
+  }
+  else if ("I love you, Grandma." === string) {
+    return lovegrandma;
+  }
+}
+
+console.log("I love you Grandma")
